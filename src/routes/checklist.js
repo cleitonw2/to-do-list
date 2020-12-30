bf62fab256cca6c5b7f27b3179b87d7e0f671f76
Cleiton <cleitonwoycik@outlook.com>
2020-12-30 15:53:21 -0300

    models e routes

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,29 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
+const Checklist = require('../models/checklist');
+
 router.get('/', (req, res) => {
     console.log('checklist');
     res.send();
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).json(req.body);
+router.post('/', async (req, res) => {
+    let { name } = req.body
+
+    try {
+        let checklist = await Checklist.create({ name })
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 });
 
 router.get('/:id', (req, res) => {
-   console.log(req.params);
-   res.send(`ID: ${req.params.id}`);
+    console.log(req.params);
+    res.send(`ID: ${req.params.id}`);
 });
 
 router.put('/:id', (req, res) => {
     console.log(req.params);
     res.send(`PUT ID: ${req.params.id}`);
- });
+});
 
- router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     console.log(req.params);
     res.send(`DELETE ID: ${req.params.id}`);
- });
+});
 
 module.exports = router
