6e74fbb87917f4880c40334e4ae06bbaf30b226b
Cleiton <cleitonwoycik@outlook.com>
2020-12-30 21:40:58 -0300

    adicionando o bulma css

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ const rootRouter = require('./src/routes');
 require('./config/database');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
-app.set('views', path.join(__dirname, './src/views'));
-app.set('view engine', 'ejs');
-
 app.use('/', rootRouter)
 
 app.use('/checklists', checkListRouter)
+
+
+app.set('views', path.join(__dirname, './src/views'));
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
     console.log('servidor rodando na porta 3000');
