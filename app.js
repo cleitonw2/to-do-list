4a7dc4b09c0ca5b236912ce9ce947542ecf6a873
Cleiton <cleitonwoycik@outlook.com>
2020-12-30 21:22:24 -0300

    adicionando ejs

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const checkListRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes');
+
 require('./config/database');
 
 const app = express();
 app.use(express.json());
+
+app.set('views', path.join(__dirname, './src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRouter)
 
 app.use('/checklists', checkListRouter)
 
