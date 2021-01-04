56cda45b5389422d7618a9208f8601e88aa5cdb0
Cleiton <cleitonwoycik@outlook.com>
2021-01-04 13:45:00 -0300

    enviando dados de um form com post

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ require('./config/database');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', rootRouter)
 
 app.use('/checklists', checkListRouter)
 
 
-app.set('views', path.join(__dirname, './src/views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
